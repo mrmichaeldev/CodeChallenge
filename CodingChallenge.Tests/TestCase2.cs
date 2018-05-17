af9455d861e6356192d223600df86eedb0b58c04
mrmichaeldev <mrmichaeldev@gmail.com>
2018-05-17 00:47:22 -0400

    Add remaining two test cases.

--- CodingChallenge.Tests/TestCase2.cs
+++ CodingChallenge.Tests/TestCase2.cs
@@ -15,13 +15,47 @@ namespace CodingChallenge.Tests
         private readonly ICardType MCCardType = new MCCardType();
 
         private CreditCard _discoverCard, _visaCard, _mcCard;
+        private IWallet _wallet1, _wallet2;
 
         private IPerson _person;
 
         [TestInitialize]
         public void Init()
         {
+            _discoverCard = new MockCreditCard(DiscoverCardType);
+            _visaCard = new MockCreditCard(VisaCardType);
+            _mcCard = new MockCreditCard(MCCardType);
+
+            _wallet1 = new Wallet();
+            _wallet1.CreditCards.Add(_discoverCard);
+            _wallet1.CreditCards.Add(_visaCard);
+
+            _wallet2 = new Wallet();
+            _wallet2.CreditCards.Add(_mcCard);
+
             _person = new Person();
+            _person.Wallets.Add(_wallet1);
+            _person.Wallets.Add(_wallet2);
+        }
+
+        [TestMethod]
+        public void Wallet1Interest()
+        {
+            Assert.AreEqual(_wallet1.CalculateTotalInterest(), _discoverCard.CalculateInterest() +
+                _visaCard.CalculateInterest());
+        }
+
+        [TestMethod]
+        public void Wallet2Interest()
+        {
+            Assert.AreEqual(_wallet2.CalculateTotalInterest(), _mcCard.CalculateInterest());
+        }
+
+        [TestMethod]
+        public void PersonInterest()
+        {
+            Assert.AreEqual(_person.CalculateTotalInterest(), _wallet1.CalculateTotalInterest() +
+                _wallet2.CalculateTotalInterest());
         }
     }
 }
---
+++ CodingChallenge.Tests/TestCase3.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingChallenge.Core;
+using CodingChallenge.Interfaces;
+using CodingChallenge.Core.CardTypes;
+
+namespace CodingChallenge.Tests
+{
+    [TestClass]
+    public class TestCase3
+    {
+        private readonly ICardType DiscoverCardType = new DiscoverCardType();
+        private readonly ICardType VisaCardType = new VisaCardType();
+        private readonly ICardType MCCardType = new MCCardType();
+
+        private CreditCard _mcCard1, _mcCard2, _mcCard3, _visaCard;
+        private IWallet _wallet1, _wallet2;
+        private IPerson _person1, _person2;
+
+
+        [TestInitialize]
+        public void Init()
+        {
+            _mcCard1 = new MockCreditCard(MCCardType);
+            _mcCard2 = new MockCreditCard(MCCardType);
+            _mcCard3 = new MockCreditCard(MCCardType);
+            _visaCard = new MockCreditCard(VisaCardType);
+
+            _wallet1 = new Wallet();
+            _wallet1.CreditCards.Add(_mcCard1);
+            _wallet1.CreditCards.Add(_mcCard2);
+
+            _person1 = new Person();
+            _person1.Wallets.Add(_wallet1);
+
+            _wallet2 = new Wallet();
+            _wallet2.CreditCards.Add(_mcCard3);
+            _wallet2.CreditCards.Add(_visaCard);
+
+            _person2 = new Person();
+            _person2.Wallets.Add(_wallet2);
+        }
+
+        [TestMethod]
+        public void Wallet1Interest()
+        {
+            Assert.AreEqual(_wallet1.CalculateTotalInterest(), _mcCard1.CalculateInterest() +
+                _mcCard2.CalculateInterest());
+        }
+
+        [TestMethod]
+        public void Wallet2Interest()
+        {
+            Assert.AreEqual(_wallet2.CalculateTotalInterest(), _mcCard3.CalculateInterest() +
+                _visaCard.CalculateInterest());
+        }
+
+        [TestMethod]
+        public void Person1Interest()
+        {
+            Assert.AreEqual(_person1.CalculateTotalInterest(), _wallet1.CalculateTotalInterest());
+        }
+
+        [TestMethod]
+        public void Person2Interest()
+        {
+            Assert.AreEqual(_person2.CalculateTotalInterest(), _wallet2.CalculateTotalInterest());
+        }
+    }
+}
